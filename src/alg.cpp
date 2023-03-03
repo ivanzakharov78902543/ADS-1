630b2e7becdc5c5edde6b17fbae4fb61fddfdd29
ivanzakharov78902543 <125127109+ivanzakharov78902543@users.noreply.github.com>
2023-03-03 15:53:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -15,8 +15,7 @@ bool checkPrime(uint64_t value) {
     }
     if (k == 0) {
         return true;
-    }
-    else {
+    } else {
         return false;
     }
 }
@@ -26,8 +25,7 @@ uint64_t nPrime(uint64_t n) {
     int l = 1;
     if (n == 1) {
         return 2;
-    }
-    else {
+    } else {
         for (int i = 3; i < 10000; i++) {
             for (int j = 2; j <= i / 2; j++) {
                 if (i % j == 0) {
@@ -36,8 +34,7 @@ uint64_t nPrime(uint64_t n) {
             }
             if (w == 0) {
                 l += 1;
-            }
-            else {
+            } else {
                 w = 0;
             }
             if (l == n) {
